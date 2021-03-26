3d2247a3ad9c5d7bd8843023cff09a3f851bcfb3
Rusty Mabiala <rustymabiala150@gmail.com>
2021-03-26 18:47:51 +0200

    second commit about maven Assignment (Group L. Adecel)

--- src/main/java/za/ac/cput/mavenproject/MarvenClass.java
+++ src/main/java/za/ac/cput/mavenproject/MarvenClass.java
@@ -11,6 +11,6 @@ package za.ac.cput.mavenproject;
  */
 public class MarvenClass {
     public static void main(String[] args) {
-        System.out.println("maven");
+        System.out.println("maven adp2");
     }
 }
